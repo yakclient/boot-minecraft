0d118060d01141746693d3dc391470a2c1b4ea39
durganmcbroom <durganmcbroom@gmail.com>
2023-07-14 22:46:36 -0700

    Added devmode option to take ext-loader from maven local.

--- src/main/kotlin/net/yakclient/client/Main.kt
+++ src/main/kotlin/net/yakclient/client/Main.kt
@@ -26,6 +26,7 @@ public fun main(args: Array<String>) {
     val workingDir by parser.option(ArgType.String, "working-dir", "w").default(System.getProperty("user.dir"))
     val mcVersion by parser.option(ArgType.String, "mcVersion", "v").required()
     val accessToken by parser.option(ArgType.String, "accessToken").required()
+    val devMode by parser.option(ArgType.Boolean, "devmode").default(false)
 
     parser.parse(args)
     val pwdPath = Path.of(workingDir)
@@ -33,21 +34,30 @@ public fun main(args: Array<String>) {
     val boot = ProductionBootInstance(pwdPath, DependencyTypeProvider())
     val request = SoftwareComponentArtifactRequest("net.yakclient.components:ext-loader:1.0-SNAPSHOT")
     if (!boot.isCached(request.descriptor)) boot.cache(
-            request,
-            SoftwareComponentRepositorySettings.default(
+        request,
+        if (devMode) SoftwareComponentRepositorySettings.local() else
+            SoftwareComponentRepositorySettings
+                .default(
                     "http://maven.yakclient.net/snapshots",
                     preferredHash = HashType.SHA1
-            )
+                )
     )
 
-    val factory = boot.componentGraph.get(request.descriptor).orNull()?.factory!! as ComponentFactory<ComponentConfiguration, ComponentInstance<ComponentConfiguration>>
+    val factory = boot.componentGraph.get(request.descriptor)
+        .orNull()?.factory!! as ComponentFactory<ComponentConfiguration, ComponentInstance<ComponentConfiguration>>
 
-    val extensions = if (System.`in`.available() == 0) listOf() else ObjectMapper().registerModule(KotlinModule.Builder().build()).readValue<List<Map<String, Any>>>(System.`in`)
-    val config = factory.parseConfiguration(ContextNodeTypes.newValueType(mapOf(
-            "mcVersion" to mcVersion,
-            "mcArgs" to listOf("--version", mcVersion, "--accessToken", accessToken),
-            "extensions" to extensions
-    )))
+    val extensions =
+        if (System.`in`.available() == 0) listOf() else ObjectMapper().registerModule(KotlinModule.Builder().build())
+            .readValue<List<Map<String, Any>>>(System.`in`)
+    val config = factory.parseConfiguration(
+        ContextNodeTypes.newValueType(
+            mapOf(
+                "mcVersion" to mcVersion,
+                "mcArgs" to listOf("--version", mcVersion, "--accessToken", accessToken),
+                "extensions" to extensions
+            )
+        )
+    )
 
     val instance = factory.new(config)
 
